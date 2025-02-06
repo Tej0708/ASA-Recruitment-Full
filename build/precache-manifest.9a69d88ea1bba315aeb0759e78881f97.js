ea8386e2f3667f5eb0cef2ac98aac46d4d2c8b09
Tej Gaonkar <tej.gaonkar@gmail.com>
2025-02-05 18:54:25 -0800

    Add build folder for deployment

---
+++ build/precache-manifest.9a69d88ea1bba315aeb0759e78881f97.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcc14daec428d5d1f5e92328b18f547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e125e054e08960991a78",
+    "url": "/static/css/2.7f5860a0.chunk.css"
+  },
+  {
+    "revision": "0eaaa92ef04332961a5b",
+    "url": "/static/css/main.e29dc8ea.chunk.css"
+  },
+  {
+    "revision": "e125e054e08960991a78",
+    "url": "/static/js/2.5862cf76.chunk.js"
+  },
+  {
+    "revision": "91d7717275128f0f7a73938e8c9407ab",
+    "url": "/static/js/2.5862cf76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4982dbc97c8ee6adbf",
+    "url": "/static/js/3.d7e6a170.chunk.js"
+  },
+  {
+    "revision": "0eaaa92ef04332961a5b",
+    "url": "/static/js/main.c042cedf.chunk.js"
+  },
+  {
+    "revision": "598d690a723da1a6da63",
+    "url": "/static/js/runtime-main.0530f95c.js"
+  },
+  {
+    "revision": "830ff36b57bdbbb04386685f623c0bb5",
+    "url": "/static/media/Bains2023.830ff36b.jpg"
+  },
+  {
+    "revision": "cdde0fb0f3c5286d0e6fcb0a549a0d6d",
+    "url": "/static/media/Bains2024.cdde0fb0.jpg"
+  },
+  {
+    "revision": "2ba7f2b490ba44e1bc4158a224401b2f",
+    "url": "/static/media/Bearden2023.2ba7f2b4.jpg"
+  },
+  {
+    "revision": "5d869a5d86b2e111719b9bcca27e6d9f",
+    "url": "/static/media/Bearden2024.5d869a5d.jpg"
+  },
+  {
+    "revision": "15b24d7457eaedfaa9efee1a327716f3",
+    "url": "/static/media/Gale2023.15b24d74.jpg"
+  },
+  {
+    "revision": "d52addb736cce2ebd6b357297c8e63eb",
+    "url": "/static/media/Gale2024.d52addb7.jpg"
+  },
+  {
+    "revision": "bf6ad837b33ecb165aa4c7f1bd43afd4",
+    "url": "/static/media/Gbatu2023.bf6ad837.jpg"
+  },
+  {
+    "revision": "2f3a8934f4e1f41ad2e1dcb0fcd91344",
+    "url": "/static/media/Gbatu2024.2f3a8934.jpg"
+  },
+  {
+    "revision": "9ad185674470ad374b33c5d0e36b9028",
+    "url": "/static/media/Hutton2023.9ad18567.jpg"
+  },
+  {
+    "revision": "e53bcfe7ad1df0b70b3d0637ee8c14cb",
+    "url": "/static/media/Hutton2024.e53bcfe7.jpg"
+  },
+  {
+    "revision": "e501ce13c8e54eeb7ac83bddb443b97f",
+    "url": "/static/media/Kennedy2023.e501ce13.jpg"
+  },
+  {
+    "revision": "26387535373a06dfa7c950bfce9d2320",
+    "url": "/static/media/Kennedy2024.26387535.jpg"
+  },
+  {
+    "revision": "9694b751963204db4f3e0aa825a82b19",
+    "url": "/static/media/Larison2023.9694b751.jpg"
+  },
+  {
+    "revision": "7af600bd2dbb7b0482a507abfd76ad40",
+    "url": "/static/media/Larison2024.7af600bd.jpg"
+  },
+  {
+    "revision": "590bd1355bf0897632a840cc22d1c5c9",
+    "url": "/static/media/Meynell2023.590bd135.jpg"
+  },
+  {
+    "revision": "d5bc963bcb7ecd49f5d913ab3cdf73f4",
+    "url": "/static/media/Meynell2024.d5bc963b.jpg"
+  },
+  {
+    "revision": "894b97aefe3981ce9bad39ed18ddd183",
+    "url": "/static/media/Sarac2023.894b97ae.jpg"
+  },
+  {
+    "revision": "5bfe862d776e2040d48d99926040b19a",
+    "url": "/static/media/Sarac2024.5bfe862d.jpg"
+  },
+  {
+    "revision": "e204e5714efe7ae192f1243dc59229ba",
+    "url": "/static/media/Tompkins2023.e204e571.jpg"
+  },
+  {
+    "revision": "373d8f1e64983f4d0e20368343948fb3",
+    "url": "/static/media/Tompkins2024.373d8f1e.jpg"
+  },
+  {
+    "revision": "af4069bbb8a3dc7c2bd70c4168a6f91a",
+    "url": "/static/media/Williams2023.af4069bb.jpg"
+  },
+  {
+    "revision": "b7969e8cb3413f01de25b80c33651e06",
+    "url": "/static/media/Williams2024.b7969e8c.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
